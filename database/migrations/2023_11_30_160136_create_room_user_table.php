d1e5f0726f0be115a53ac76062b6ad6346223424
Borja <pintocorreas@gmail.com>
2023-11-30 17:10:29 +0100

    feat: created relation between user and room table at room_user table

--- database/migrations/2023_11_30_160136_create_room_user_table.php
+++ database/migrations/2023_11_30_160136_create_room_user_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
     {
         Schema::create('room_user', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('room_id')->constrained();
-            $table->unsignedBigInteger('user_id')->constrained();
+            $table->unsignedBigInteger('room_id');
+            $table->unsignedBigInteger('user_id');
             $table->timestamp('created_at')->useCurrent();
-            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();   
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();  
+            
+            $table->foreign('room_id')->references('id')->on('rooms');
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
