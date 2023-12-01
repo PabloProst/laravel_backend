05abf830ba0d375e3bbe45dfee2da3c9b9e3b641
Borja <pintocorreas@gmail.com>
2023-12-01 09:55:44 +0100

    feat: created auth controller, register endpoint

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Error;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+use Laravel\Sanctum\PersonalAccessToken;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        try {
+            $validator = $this->validateDataUser($request);
+
+            if ($validator->fails()) {
+                return response()->json(
+                    [
+                        "success" => false,
+                        "message" => "Error registering user",
+                        "error" => $validator->errors()
+                    ],
+                    Response::HTTP_BAD_REQUEST
+                );
+            }
+
+            $newUser = User::create(
+                [
+                    "name" => $request->input('name'),
+                    "email" => $request->input('email'),
+                    "password" => bcrypt($request->input('password'))
+                ]
+            );
+
+            return response()->json(
+                [
+                    "success" => true,
+                    "message" => "user registered",
+                    "data" => $newUser
+                ],
+                Response::HTTP_CREATED
+            );
+        } catch (\Throwable $th) {
+            Log::error($th->getMessage());
+
+            return response()->json(
+                [
+                    "success" => false,
+                    "message" => "Error registering user"
+                ],
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+
+    private function validateDataUser(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|min:3|max:255',
+            'email' => 'required|unique:users|email',
+            'password' => 'required|min:6|max:12',
+        ]);
+
+        return $validator;
+    }
+}
